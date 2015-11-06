fb1e235bfedfaa5c795278386c22b0007abf7abc
lhc1456 <lsch1456@gmail.com>
2015-11-06 18:31:08 +0900

    151106

--- 151106/main.c
+++ 151106/main.c
@@ -10,7 +10,7 @@ main()
   int sum_x = 0, sum_y = 0, avg_x, avg_y;
 
   int x, y;
-  int check = 0;
+  int check = 0;//랜덤 개체군을 뽑을 것인지와 반복문을 빠져나가기 위한 값
 
   int board[15][15] = {0};
 
@@ -19,7 +19,7 @@ main()
   while(1)
   {
     sum_x = 0; sum_y = 0;
-    if(check == 0 || check ==1)
+    if(check == 0 || check ==1)//0과 1이외의 값일때에는 염색체 값을 초기화 하지 않는다.
     {
       rand_population(pop);//랜덤으로 개체군의 염색체 값 초기화
     }
@@ -60,16 +60,24 @@ main()
 
     printf("x : ");
     scanf("%d", &x);
-    fflush(stdin);
+    if(x == -9999)
+    {
+      printf("종료합니다\n");
+      break;
+    }
     if(x<0 || x>14)
     {
       printf("ERROR\n");
-      check = 2;
+      check = 2;//범위를 넘겼을 경우 check를 2로 주며 다시 반복문을 시행한다.
       continue;
     }
     printf("y : ");
     scanf("%d", &y);
-    fflush(stdin);
+    if(y == -9999)
+    {
+      printf("종료합니다\n");
+      break;
+    }
     if(y<0 || y>14)
     {
       printf("ERROR\n");
@@ -80,7 +88,7 @@ main()
     if(avg_x == x && avg_y == y)
     {
       printf("ERROR\n");
-      check = 2;
+      check = 2;//컴퓨터의 값과 겹치는 경우 check를 2로 주며 다시 반복문을 시행한다.
       continue;
     }
 
@@ -92,7 +100,7 @@ main()
         if(board[y][x] == 'X' || board[y][x] == 'O')
 	{
 	  printf("ERROR\n");
-	  check = 2;
+	  check = 2;//입력한 값이 판 위에 존재한다면 check를 2로 주고 다시 반복문을 시행한다.
 	  break;
 	}
       }
@@ -106,7 +114,7 @@ main()
       continue;
     }
 
-    board[avg_y][avg_x] = 'X';
+    board[avg_y][avg_x] = 'X';//모든 예외처리가 끝나면 board에 값을 집어넣는다. 상대와 나를 구분하기위해X와O로 나누어 값을 준다.
     board[y][x] = 'O';
 
 /*    printf("   ");
