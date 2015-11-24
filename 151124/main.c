a273bdaca43b871bc960c94dd80a13a0b75cce11
lhc1456 <lsch1456@gmail.com>
2015-11-24 15:32:19 +0900

    151124

--- 151124/main.c
+++ 151124/main.c
@@ -9,7 +9,7 @@ main()
 
   int board[15][15] = {0};
 
-  population *pop = create_population(pop_size, bit_count, data_count);//¿¿¿ ¿¿ 
+  population *pop = create_population(pop_size, bit_count, data_count);//¿¿¿ ¿¿
 
   play_board(pop, board);//¿¿¿
 
