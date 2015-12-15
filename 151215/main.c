5b26f443ee6371dcccc0b04088969263cc79d89a
lhc1456 <lsch1456@gmail.com>
2015-12-15 19:43:31 +0900

    151215

---
+++ 151215/main.c
@@ -0,0 +1,27 @@
+﻿#include "GA.h"
+
+main()
+{
+  int i, j,
+      pop_size = 10,//개체 수
+      bit_count = 2,//염색체 내의 비트 수
+      data_count = 2;//염색체 내의 데이터 수
+
+  int board[15][15];
+  
+  //board 배열 안의 모든 값을 ' '으로 초기화 해준다.
+  for(i=0; i<15; i++)
+  {
+    for(j=0; j<15; j++)
+    {
+      board[i][j] = ' ';
+    }
+  }
+
+  population *pop = create_population(pop_size, bit_count, data_count);//개체군 생성
+
+  play_board(pop, board);//플레이!
+
+  free_population(pop);
+}
+
